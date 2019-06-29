100f5967f21e47fe490a6ec7608d38e56e244b65
Gabriel Naves da Silva <gabri.navess@gmail.com>
2019-06-29 17:58:24 -0300

    KeyboardPlayer class can now play an entire octave

--- Assets/Scripts/KeyboardPlayer.cs
+++ Assets/Scripts/KeyboardPlayer.cs
@@ -1,28 +1,53 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Synth
 {
     public class KeyboardPlayer : MonoBehaviour
     {
         public Instrument[] instruments;
-        public double frequency;
+        public double baseFrequency;
 
-        private Note note;
+        private KeyCode[] keys = new KeyCode[] {
+            KeyCode.Z,
+            KeyCode.S,
+            KeyCode.X,
+            KeyCode.D,
+            KeyCode.C,
+            KeyCode.V,
+            KeyCode.G,
+            KeyCode.B,
+            KeyCode.H,
+            KeyCode.N,
+            KeyCode.J,
+            KeyCode.M,
+            KeyCode.Comma
+        };
+        private Note[] notes;
+        private double d12thRootOf2 = Math.Pow(2.0, 1.0 / 12.0);
 
-        private void Update() {
-            if (Input.GetKeyDown(KeyCode.Z))
+        private void Awake()
+        {
+            notes = new Note[keys.Length];
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < keys.Length; ++i)
             {
-                if (note != null)
-                    note.TurnOff();
-
-                note = new Note(frequency);
-                foreach (var instrument in instruments)
-                    instrument.AddNote(note);
-            }
-            if (note != null && !Input.GetKey(KeyCode.Z))
-            {
-                note.TurnOff();
-                note = null;
+                if (Input.GetKeyDown(keys[i]))
+                {
+                    if (notes[i] != null)
+                        notes[i].TurnOff();
+                    notes[i] = new Note(baseFrequency * Math.Pow(d12thRootOf2, i));
+                    foreach (var instrument in instruments)
+                        instrument.AddNote(notes[i]);
+                }
+                else if (notes[i] != null && !Input.GetKey(keys[i]))
+                {
+                    notes[i].TurnOff();
+                    notes[i] = null;
+                }
             }
         }
     }
